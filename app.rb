118733817d822a039cbb142953a700a74b55eaba
Jason D. Shatz <jshatz@optonline.net>
2016-07-01 15:02:54 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -32,20 +32,23 @@ post "/signup" do
 	User.create(:username => params[:username], :password => params[:password])
 	redirect :profiles
 	  @user = User.where(username: params[:username]).first
-	  if @user == null 
-	  	redirect '/signup'
+	  if @user != null && @user.password != null 
+	  	redirect '/'
 	  elsif @user.username != params[:username]
 	  	redirect '/signup'
-	  else
-		  if @user.password == params[:password]
-		    redirect '/'
-		  else
-		    redirect '/signup'
-		  end
+	  	flash[:notice] = "Sign-up failed." #Do we have the sinatra-flash gem set up?
 	end
 end
 
-
+#Sign-in button/activate session
+post "/signin" do
+	  if @user.username == params[:username] && @user.password == params[:password] 
+	  	redirect '/'
+	  else
+		redirect '/signin' #Is this redundant, if we don't have a sign-in page?
+		flash[:notice] = "Sign-in failed."
+	end
+end
 
 # when user clicks the upload_post buttom
 post "/blog" do
