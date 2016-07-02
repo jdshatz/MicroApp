62a403ed7b30ecbe8aafa49347dd926457953480
Virginia Rucinski <rucinski9@gmail.com>
2016-07-01 23:23:26 -0400

    Added login and logout to navbar, amended css, added form to myprofile.erb, amended app.rb but commented out old so Jason and Nuri can see what I changed of app.rb

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "sinatra"
 require "sinatra/activerecord"
 require "./models"
 
-enable:sessions
+enable :sessions
 set :database, "sqlite3:database.sqlite3"
 
 get "/" do 
@@ -28,28 +28,55 @@ get "/signup" do
 end
 
 # when user clicks the signup button
-post "/signup" do
-	User.create(:username => params[:username], :password => params[:password])
-	redirect :profiles
-	  @user = User.where(username: params[:username]).first
-	  if @user != null && @user.password != null 
-	  	redirect '/'
-	  elsif @user.username != params[:username]
-	  	redirect '/signup'
-	  	flash[:alert] = "Sign-up failed."
-	end
+# post "/signup" do
+# 	User.create(:username => params[:username], :password => params[:password])
+# 	redirect :profiles
+# 	  @user = User.where(username: params[:username]).first
+# 	  if @user != null && @user.password != null 
+# 	  	redirect '/'
+# 	  elsif @user.username != params[:username]
+# 	  	redirect '/signup'
+# 	  	flash[:alert] = "Sign-up failed."
+# 	end
+# end
+
+post '/signup' do
+  User.create(
+    username: params[:username],
+    password: params[:password],
+  )
+
+  # flash[:notice] = "You have signed up"
+  # redirect "/"
+end
+
+get "/login-failed" do
+  erb :login_failed
 end
 
 #Sign-in button/activate session
-post "/signin" do
-	@user = User.where(username: params[:username]).first
-	  if @user != null && @user.password == params[:password] 
-	  	session[:user_id] = @user.id
-	  	redirect '/'
-	  else
-		redirect '/signin' #Is this redundant, if we don't have a sign-in page?
-		flash[:alert] = "Sign-in failed."
-	end
+# post "/signin" do
+# 	@user = User.where(username: params[:username]).first
+# 	  if @user != null && @user.password == params[:password] 
+# 	  	session[:user_id] = @user.id
+# 	  	redirect '/'
+# 	  else
+# 		redirect '/signin' #Is this redundant, if we don't have a sign-in page?
+# 		flash[:alert] = "Sign-in failed."
+# 	end
+# end
+
+#Sign-in button/activate session
+post "/myprofile" do
+  @user = User.where(username: params[:username]).first
+  if @user && @user.password == params[:password]
+    session[:user_id] = @user.id
+    # flash[:notice] = "You are signed in"
+    redirect "/blog"
+  else
+    # flash[:error] = "You are not signed in"
+    # redirect "/login-failed"
+  end
 end
 
 # when user clicks the upload_post buttom
